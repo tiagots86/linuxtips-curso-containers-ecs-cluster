8c990d721fc30f30be06de91c335f72398ddd3ee
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-09-02 19:42:18 -0300

    estrutura de redes ecs

---
+++ variables.tf
@@ -0,0 +1,37 @@
+#### GENERAL CONFIGS ####
+
+variable "project_name" {
+  description = "Nome do projeto. Essa variável será um prefixo para os recursos criados dentro desse projeto"
+}
+
+variable "region" {
+  default = "Região da AWS onde os recursos serão criados"
+}
+
+variable "ssm_vpc_id" {
+
+}
+
+variable "ssm_public_subnet_1" {
+
+}
+
+variable "ssm_public_subnet_2" {
+
+}
+
+variable "ssm_public_subnet_3" {
+
+}
+
+variable "ssm_private_subnet_1" {
+
+}
+
+variable "ssm_private_subnet_2" {
+
+}
+
+variable "ssm_private_subnet_3" {
+
+}
